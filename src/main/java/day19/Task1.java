c0135ef2b14009a5099646c1c289851dac58e0ba
Jenny <jen_sat@mail.ru>
2022-10-28 00:06:59 +0500

    complete day19 tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,60 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
+        public static void main(String[] args) throws FileNotFoundException {
+            File file = new File("src/main/resources/dushi.txt");
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter("[.,:;()?!„«»<>…\"\\s–]+");
 
+            Map<String, Integer> map = new HashMap<>();
+
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (!map.containsKey(word))
+                    map.put(word, 1);
+                else
+                    map.put(word, map.get(word) + 1);
+            }
+
+            int i = 0;
+            map = Sort.sortByValues(map);
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (i < 100) {
+                    System.out.println(entry.getKey() + " : " + entry.getValue());          // Чичиков - 608
+                    i++;
+                }
+                else return;
+            }
+        }
     }
-}
+    class CustomComparator<String, Integer extends Comparable> implements Comparator<String> {
+        private Map<String, Integer> map;
+
+        public CustomComparator(Map<String, Integer> map) {
+            this.map = new HashMap<>(map);
+        }
+
+        @Override
+        public int compare(String s1, String s2) {
+            return map.get(s2).compareTo(map.get(s1));
+        }
+    }
+
+    class Sort {
+        public static <String, Integer> Map<String, Integer> sortByValues(Map<String, Integer> map)
+        {
+            Comparator<String> comparator = new CustomComparator(map);
+
+            Map<String, Integer> sortedMap = new TreeMap<>(comparator);
+            sortedMap.putAll(map);
+
+            return sortedMap;
+        }
+    }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,11 +1,63 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
-    public static void main(String[] args) {
+        public static void main(String[] args) throws FileNotFoundException {
+            File file = new File("src/main/resources/taxi_cars.txt");
+            Scanner scannerCars = new Scanner(file);
 
+            Map<Integer, Point> map = new HashMap<>();
+
+            while (scannerCars.hasNextLine()) {
+                String line = scannerCars.nextLine();
+                String[] numbers = line.split(" ");
+                int[] arrNumbers = new int[3];
+
+                int i = 0;
+                for (String number : numbers) {
+                    arrNumbers[i] = Integer.parseInt(number);
+                    map.put(arrNumbers[0], new Point(arrNumbers[1], arrNumbers[2]));
+                    i++;
+                }
+            }
+            scannerCars.close();
+
+//        for (Map.Entry<Integer, Point> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
+
+            System.out.println("Введите 4 числа от 0 до 99 (координаты квадрата):");
+            Scanner scannerCoordinates = new Scanner(System.in);
+            int x1 = scannerCoordinates.nextInt();
+            int y1 = scannerCoordinates.nextInt();
+            int x2 = scannerCoordinates.nextInt();
+            int y2 = scannerCoordinates.nextInt();
+            scannerCoordinates.close();
+
+            if (x1 < 0 || x1 > 99 || x2 < 0 || x2 > 99 || y1 < 0 || y1 > 99 || y2 < 0 || y2 > 99) {
+                System.out.println("Значения координат находятся вне диапазона от 0 до 99");
+            } else {
+                int count = 0;
+                for (Map.Entry<Integer, Point> entry : map.entrySet()) {
+                    Point coordinatesCars = entry.getValue();
+                    if ((coordinatesCars.getX() > x1 && coordinatesCars.getX() < x2) &&
+                            (coordinatesCars.getY() > y2 && coordinatesCars.getY() < y1)) {
+                        System.out.println(entry.getKey());
+                        count++;
+                    }
+                }
+                if (count != 0)
+                    System.out.println("Количество машин, находящихся в заданных координатах: " + count);
+                else
+                    System.out.println("В заданных координатах нет машин.");
+            }
+        }
     }
-}
-
